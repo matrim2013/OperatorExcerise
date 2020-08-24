0a9531bd9a3870e51560a22d8c8adbf8082159d1
Lewis S <simpkinslewisa@gmail.com>
2020-08-24 16:16:31 -0400

    try this again

---
+++ OperatorExer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperatorExer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your length?");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your width?");
+            int width = int.Parse(Console.ReadLine());
+
+            var areaRect = length * width;
+            var perimeter = ((length * 2) + (width * 2));
+
+            Console.WriteLine($"The permimeter of this object runs {perimeter} and has an area of {areaRect}");
+
+            Console.WriteLine("What is the diameter of your circle?");
+            var diameter = int.Parse(Console.ReadLine());
+
+            var radius = diameter / 2;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (radius * radius);
+
+            Console.WriteLine($"The area of your circle is {areaOfCircle}");
+
+
+
+
+        }
+    }
+}
